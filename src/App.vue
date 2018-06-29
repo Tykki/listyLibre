61e497d3a40378213683c04c2befbb0c9334e7a2
Tykki <ayiri1401@gmail.com>
2018-06-29 17:06:22 -0500

    json messages fix

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,74 @@
+
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div id="main-app">
+    <img src="./assets/libra.svg">
+
+    <add-appointment @addReq="appAdd"/>
+    <search-appointment :filKey="filterKey" :filDir="filterDir" @searchRec="searchApts" @keyChange="changeKey" @dirChange="changeDir"/>
+    <appointment-list
+      :appointments = 'filteredApts' @remove="removeItem"/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
-</script>
+  /* eslint-disable */
+import _ from 'lodash'
+import moment from 'moment'
+import AppointmentList from '@/components/AppointmentList.vue';
+import AddApp from '@/components/AddAppointment.vue'
+import SearchApp from '@/components/SearchAppointment.vue'
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+export default {
+  name: 'MainApp',
+  data() {
+    return {
+      theAppointments: [],
+      searchTerms: '',
+      filterKey: 'petName',
+      filterDir: 'asc'
+    } //return
+  }, //data
+
+  components: {
+    'appointment-list': AppointmentList,
+    'add-appointment': AddApp,
+    'search-appointment': SearchApp
+  }, //components
+
+  created: function() {
+    $.getJSON('/static/appointments.json')
+      .done( info =>  {
+        this.theAppointments = info;
+    }); //getJSON
+  }, //created
+  methods: {
+    appAdd(apt){
+      this.theAppointments.push(apt)
+    },
+    removeItem(apt){
+      this.theAppointments = _.without(this.theAppointments, apt)
+    },
+    searchApts(terms){
+      this.searchTerms = terms
+    },
+    changeKey(key){
+      this.filterKey = key
+    },
+    changeDir(dir){
+      this.filterDir = dir
+    }
+  },
+  computed: {
+    searchedApt(){
+      return this.theAppointments.filter(item => 
+        item.petName.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+        item.petOwner.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+        item.aptNotes.toLowerCase().match(this.searchTerms.toLowerCase())
+        )
+    },
+    filteredApts(){
+      return _.orderBy(this.searchedApt, item => item[this.filterKey].toLowerCase(), this.filterDir)
+    }
+  }
+} //default
+</script>
